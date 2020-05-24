0ded9a8447a1801f6a5e963e95d87b94668ff4c0
shawn-dolifka <shawn.dolifka@gmail.com>
2020-05-24 01:35:43 -0500

    Updated Tree with Height functions

--- tree.py
+++ tree.py
@@ -57,4 +57,32 @@ class Tree:
         '''Return True if the tree is empty'''
         return len(self) == 0
 
-        
+    def depth(self, p):
+        '''Return the number of levels separating Position p from the root'''
+        if self.is_root(p):
+            return 0
+        else:
+            return 1 + self.depth(self.parent(p))
+
+    #Less efficient height method, O(n^2) worst case
+    def _height1(self):
+        '''Return the height of the tree.'''
+        return max(self.depth(p) for p in self.positions() if self.is_leaf(p))
+
+    #Efficent height method, O(n) worst case
+    def _height2(self, p):
+        '''Return the height of the subtree rooted at Position p.'''
+        if self.is_leaf(p):
+            return 0
+        else:
+            return 1 + max(self._height2(c) for c in self.children(p))
+
+    #Public method for tree height, either entire tree or at Position p
+    def height(self, p=None):
+        '''
+        Return the height of the subtree rooted at Position p.
+        If p is None, return the height of the entire tree.
+        '''
+        if p is None:
+            p = self.root()
+        return self._height2(p) #Start the _height2 recursion
